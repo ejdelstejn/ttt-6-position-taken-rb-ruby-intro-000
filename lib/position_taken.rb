595293d090fccf460c642c450a6874459a072c39
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-01-21 01:29:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,12 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == ""
+  if board[index] == " "
     false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  else
+    true
+  end
 end
